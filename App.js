561f82c1a83aff59e8632ab8fc6d7b9e9e1ee757
siva-natarajan <siva-natarajan>
2021-05-24 10:27:12 +0530

    Updates

---
+++ App.js
@@ -0,0 +1,37 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+var ReactDOM = __importStar(require("react-dom"));
+var React = __importStar(require("react"));
+var Footer_1 = require("./containers/Footer");
+var App = /** @class */ (function () {
+    function App() {
+        this.render();
+    }
+    App.prototype.render = function () {
+        ReactDOM.render(React.createElement(Footer_1.Footer, { app: this }), document.getElementById("root"));
+    };
+    return App;
+}());
+exports.App = App;
+new App();
+//# sourceMappingURL=App.js.map
